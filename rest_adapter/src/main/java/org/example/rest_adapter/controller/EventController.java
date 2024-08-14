21f4b9b1da1854929c86090189a629e6aaf8a1a6
JaberKh <j.khaloua@hotmail.com>
2024-08-14 09:20:44 +0200

    Utilisation des modules et ajout des dependances

--- rest_adapter/src/main/java/org/example/rest_adapter/controller/EventController.java
+++ rest_adapter/src/main/java/org/example/rest_adapter/controller/EventController.java
@@ -3,6 +3,7 @@ package org.example.rest_adapter.controller;
 
 import org.example.dto.EventDTO;
 import org.example.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ import java.util.List;
 public class EventController {
 
     private final EventService eventService;
-
+    
     public EventController(EventService eventService) {
         this.eventService = eventService;
     }
